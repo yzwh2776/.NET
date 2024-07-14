8591f9881c5433537ba2c90679b80afea0f7a3cb
Steven <spakiela99@gmail.com>
2024-07-13 19:51:19 -0700

    Update to Set class

--- Math/Set/Program.cs
+++ Math/Set/Program.cs
@@ -1,7 +1,5 @@
-﻿using System.IO;
+﻿class Text {
 
-class Text {
-	
 	public static void PRINT(string text) {
 
 		char[] chars= text.ToCharArray();
@@ -10,7 +8,7 @@ class Text {
 
 			Console.Write(chars[i]);
 			System.Threading.Thread.Sleep(30);
-
+		
 			}
 
 		Console.WriteLine();
@@ -22,7 +20,7 @@ class Text {
 
 		char[] chars= text.ToCharArray();
 
-		for (int i= 0; i < text.Length; i++) {
+		for (int i= 0; i < text.Length; i++) {	
 
 			Console.Write(chars[i]);
 			System.Threading.Thread.Sleep(10);
@@ -35,343 +33,112 @@ class Text {
 		}
 
 	}
-	
+
+
 class Set {
 
-	public string set; 
 	public string elements;
+	public string set;	
 
 	public Set() {
 
 		Text.COMMAND("\nRUN_Set();");
+		Text.PRINT("\n	Generating new set...");	
 
-		set= "{}";
-		elements= ""; 
+		this.elements= "";
+		this.set= "{}";
 
-		Text.PRINT("\n	Generating new Set...");
 		Text.PRINT("	Successfully created new set!");
 		Text.COMMAND($"\nPRINT_Set();");
 		Text.COMMAND($"{this.set}");
-
+		
 		}
 
 	public Set(string arg) {
+	
+		Text.COMMAND($"\nRUN_Set({arg});");
+		Text.PRINT("\n	Generating new set...");
 
-		Text.COMMAND($"\nRUN_Set(string \"{arg}\");");
+		this.elements= arg;
+		this.set= "{" + arg + "}";
 
-		elements= arg;
-		set= "{" + arg + "}";
-
-		Text.PRINT("\n	Generating new Set...");
 		Text.PRINT("	Successfully created new set!");
 		Text.COMMAND($"\nPRINT_Set();");
 		Text.COMMAND($"{this.set}");
-
-		}
-
-	public Set Remove_Element(Set a) {
-
-		Text.COMMAND($"\nRUN_Remove_Element();");
-
-		Text.PRINT($"\n	Running Remove_Element()...");
-
-		int start_pos;
-		int length;	
-
-		start_pos= this.set.IndexOf(a.set);
-		length= a.set.Length;
-
-		Text.PRINT($"	Attempting to remove {a.set} from {this.set}...");
-		
-		try {
-
-			Text.PRINT("	Trying...");
-			Text.PRINT($"	Removing {this.set.Substring(start_pos, length)}...");
-			this.set= this.set.Remove(start_pos, length + 2);
-		
-			Text.PRINT($"	Successfully removed {a.set}!");
-			Text.COMMAND($"\nPRINT_Set();");
-			Text.COMMAND($"{this.set}");
-		
-			}
-
-		catch {
-
-			Text.PRINT("	Caught Exception!");
-			Text.PRINT($"	Removing {this.set.Substring(start_pos, length)}...");
-			this.set= this.set.Remove(start_pos - 2, length + 2);
-
-			Text.PRINT($"	Successfully removed {a.set}!");
-			Text.COMMAND($"\nPRINT_Set();");
-			Text.COMMAND($"{this.set}");
-			
-			}
-
-		finally {	
-
-			start_pos= this.elements.IndexOf(a.elements);
-			length= a.elements.Length;
 	
-			Text.PRINT($"\n	Validating contents of {this.set}...");
-
-			try {
-
-				Text.PRINT("	Trying...");
-				Text.COMMAND($"	Removing {this.elements.Substring(start_pos - 1, length + 4)}...");
-				this.elements= this.elements.Remove(start_pos - 1, length + 4);
-				Text.COMMAND($"\nPRINT_Elements();");
-				Text.COMMAND($"{this.elements}");
-
-				}
-			
-			catch {
-
-				Text.PRINT("	Caught Exception!");
-				Text.PRINT($"	Removing {this.elements.Substring(start_pos - 1, length + 2)}...");
-				this.elements= this.elements.Remove(start_pos - 3, length + 4);
-				Text.COMMAND($"\nPRINT_Elements();");
-				Text.COMMAND($"{this.elements}");
-
-				}
-
-			}
-
-		Text.PRINT("\n	Contents validated!");
-		return this;
-
 		}
-		
-	public Set Do_PairSet(Set a) {
 
-		Text.COMMAND($"\nRUN_Do_PairSet();");
+	public static Set PairSet(Set x, Set y) {
 
-		Text.PRINT("\n	Running Do_PairSet()...");
+		Text.COMMAND($"\nRUN_PairSet({x.set},{y.set});");
+		Text.PRINT("\n	Running PairSet()...");
 
-		Set TEMP= new Set();
+		if (x.set == y.set) {
 
-		if (this.elements == a.elements) {
-
-			TEMP.elements= this.set;
-			TEMP.set= "{" + TEMP.elements + "}";
+			Set xy= new Set(x.set);
+			return xy;
 
 			}
 
 		else {
 
-			TEMP.elements= a.set + ", " + this.set;
-			TEMP.set= "{" + TEMP.elements + "}";
+			Set xy= new Set(x.set + "," + y.set);
+			return xy;
 
 			}
 
-		Text.COMMAND($"\nPRINT_Set();");
-		Text.COMMAND($"{TEMP.set}");
-	
-		return TEMP;
-
 		}
 
-	public static Set Get_NaturalNumbers(int n) {
+	public static Set UnionSet(Set x) {
 
-		Text.COMMAND($"\nRUN_Get_NaturalNumbers({n});");
-		Text.PRINT($"\n	Running Get_NaturalNumbers({n})...");
+		Text.COMMAND($"\nRUN_UnionSet({x.set});");
+		Text.PRINT("\n	Running UnionSet()...");
+
+		Set Ux= new Set();
+		Ux.elements= x.elements;
+		Ux.set= x.elements;
 		
+		return Ux;
+	
+		}
+
+	public static Set UnionSet(Set x, Set y) {
+
+		Text.COMMAND($"\nRUN_UnionSet({x.set},{y.set});");
+		Text.PRINT("\n	Running UnionSet()...");
+
+		Set xUy= new Set(x.elements + "," + y.elements);
+
+		return xUy;
+
+		}
+	
+	public static Set Intersect(Set x, Set y) {
+
+		Text.COMMAND($"\nRUN_Intersect({x.set},{y.set});");
+		Text.PRINT("\n	Running Interect()...");
+
+		
+
+		
+
+	static void Main(string[] arg) {
+
 		Set Empty_Set= new Set();
-		Set N= new Set();
-		for (int i= 0; i <= n; i++) {
 
-			N= N.Do_PairSet(Empty_Set);
-
-			}
-
-		return N;
-
-		}
-
-	}
-
-
-class NOTES {
-
-	public static void Chapter_3() {
-
-		Console.WriteLine("\x1b[2J");
-		Text.PRINT("				Section 3: Removing Elements From Sets\n");
-
-		Set Hello= new Set("Hello");
-		Set World= new Set("World!");
-		Set Hello_World= World.Do_PairSet(Hello);
-
-		Text.PRINT($"\n			\"We now have three sets {Hello.set}, {World.set}, and their pair set {Hello_World.set}");
-		Text.PRINT("			We can use Remove_Element() to remove a specified element from a set.\"");
-
-		Hello_World= Hello_World.Remove_Element(Hello);
-
-		Hello_World= World.Do_PairSet(Hello);
-		Hello_World= Hello_World.Remove_Element(World);
-
-		Text.PRINT("\nEnter section number <#>, or press <enter> to continue...");
-
-		string x= Console.ReadLine();
-
-		switch (x) {
-
-		case "1":
-
-			NOTES.Chapter_1();
-			break;
-			
-		case "2":
-
-			NOTES.Chapter_2();
-			break;
-
-		case "3":
-
-			NOTES.Chapter_3();
-			break;
-
-		case "":
-
-			NOTES.Intro();
-			break;
-
-			}
-	
-		}
-
-	public static void Chapter_2() {
-
-		Console.WriteLine("\x1b[2J");
-		Text.PRINT("\n				Section 2: Pair Sets\n");
-		
 		Set X= new Set("X");
 		Set Y= new Set("Y");
 
-		Text.PRINT($"			\"{X.set} and {Y.set} are both sets with {X.elements} and {Y.elements} as elements, respectively.");
-		Text.PRINT("			Using Do_PairSet(), we can generate the pair set between two sets:\"");
+		Set XY= Set.PairSet(X, Y);
 
-		Set XY= new Set();
-		
-		XY= Y.Do_PairSet(X);
+		Set UX= Set.UnionSet(X);
+		Set XUY= Set.UnionSet(X, Y);
 
-		Text.PRINT("		\"Using Set() and Do_PairSet(), we can generate the set of natrual numbers:\"");
-
-		Set.Get_NaturalNumbers(3);	
-
-		Text.PRINT("\nEnter section number <#>, or press <enter> to continue...");
-
-		string x= Console.ReadLine();
-
-		switch (x) {
-
-		case "1":
-
-			NOTES.Chapter_1();
-			break;
-			
-		case "2":
-
-			NOTES.Chapter_2();
-			break;
-
-		case "3":
-
-			NOTES.Chapter_3();
-			break;
-
-		case "":
-
-			NOTES.Chapter_3();
-			break;
-
-			}
-	
-		}
-
-	public static void Chapter_1() {
-
-		Console.WriteLine("\x1b[2J");
-		Text.PRINT("				Section 1: Generating Sets");
-		Text.PRINT("\n			We can generate the empty set ('{}') with Set()\"\n");
-
-		Set Empty_Set= new Set();
-		Set X= new Set("X");
-
-		Text.PRINT("\nEnter section number <#>, or press <enter> to continue...");
-
-		string x= Console.ReadLine();
-
-		switch (x) {
-
-		case "1":
-
-			NOTES.Chapter_1();
-			break;
-			
-		case "2":
-
-			NOTES.Chapter_2();
-			break;
-
-		case "3":
-
-			NOTES.Chapter_3();
-			break;
-
-		case "":
-
-			NOTES.Chapter_2();
-			break;
-
-			}
-
+		Set ONE= Set.PairSet(Empty_Set, Empty_Set);
+		ONE= Set.UnionSet(ONE);
 
 		}
-	
-
-	public static void Intro() {
-
-		Console.WriteLine("\x1b[2J");
-		Text.PRINT("\n					Notes on Sets\n");
-
-		Text.PRINT("\n			Table of Contents:");
-		Text.PRINT("\n				Section 1: Generating Sets");
-		Text.PRINT("\n				Section 2: Pair Sets");
-		Text.PRINT("\n				Section 3: Removing Element From Sets\n");
-		Text.PRINT("\nEnter section number <#>, or press <enter> to continue...");
-
-		string x= Console.ReadLine();
-
-		switch (x) {
-
-		case "1":
-
-			NOTES.Chapter_1();
-			break;
-			
-		case "2":
-
-			NOTES.Chapter_2();
-			break;
-
-		case "3":
-
-			NOTES.Chapter_3();
-			break;
-
-		case "":
-
-			NOTES.Chapter_1();
-			break;
-
-			}
-
-		}
-	
-static void Main(string[] args) {
-	
-	NOTES.Intro();
 
 	}
 
-}
+		
--- Math/Set/obj/Debug/net8.0/Set.AssemblyInfo.cs
+++ Math/Set/obj/Debug/net8.0/Set.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Set")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b21bdb261ee3c4e23c81712ee796fb643d5a2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de77e4ac9549b5ad06cee098ccea07d50e180b4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Set")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Set")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
